8d1a01626490f7a2cf7badb5ca13f448d9c66f23
David Khavari <dkhavari@gmail.com>
2014-05-14 11:09:54 -0700

    Adding a few more files.

---
+++ src/classes/Constants.java
@@ -0,0 +1,17 @@
+package classes;
+
+public final class Constants {
+	
+	private Constants() {}
+	
+	// Meta-variables.
+	public static final String databaseName = "aligndb";
+	public static final String mongoClient = "mongo";
+	public static final String userCollection = "users";
+	
+	// Specific variables.
+	public static final String idField = "username";
+	public static final String nameField = "realname";
+	public static final String passwordField = "password";
+
+}
---
+++ src/listeners/ContextListener.java
@@ -0,0 +1,55 @@
+package listeners;
+
+import java.net.UnknownHostException;
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
+import com.mongodb.MongoClient;
+
+/**
+ * Application Lifecycle Listener implementation class ContextListener
+ * 
+ */
+@WebListener
+public class ContextListener implements ServletContextListener {
+
+	/*
+	 * Class variables.
+	 */
+	private MongoClient mongo;
+
+	/*
+	 * Constructor: ContextListener
+	 * ----------------------------
+	 * No setup required for the listener, so this method does nothing.
+	 */
+	public ContextListener() {}
+
+	/*
+	 * Function: contextInitialized
+	 * ----------------------------
+	 * Sets up the connection to MongoDB, puts in in the ServletContext.
+	 */
+	public void contextInitialized(ServletContextEvent arg0) {
+		// Connects to database in try-catch block, sets context.
+		try {
+			// Initializing MongoDB.
+			mongo = new MongoClient("localhost", 27017);
+			// Get the context so we can add the database to it.
+			arg0.getServletContext().setAttribute("mongo", mongo);
+		} catch (UnknownHostException e) {
+			// Throw an error if a connection issue arises (eg: mongod not running).
+			System.err.println("Connection to database failed: " + e);
+		}
+	}
+
+	/*
+	 * Function: contextDestroyed
+	 * --------------------------
+	 * Closes the MongoClient.
+	 */
+	public void contextDestroyed(ServletContextEvent arg0) {
+		mongo.close();		
+	}
+
+}
